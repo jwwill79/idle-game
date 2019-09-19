7c8a4a8f13c8687750693ee274222961140c0ddf
jimmywilliamson <jwwill79@email.com>
2019-09-19 14:32:43 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
 var score = 1; // Create a variable to store the score in
 
 var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
+var button = new Button('parts', btnPress);   // Create a button element
 var hidden = new Button('Boo!');
+var dino = new Button(score)
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
 
@@ -17,4 +18,5 @@ function btnPress() {
   if(score > 15) {
     hidden.hide(500);
   }
+  dino.edit(score);
 }
